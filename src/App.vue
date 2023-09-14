e2e0dd2716e29cfd76bd6ec814415408ecbeedfd
plastikSun <marcom2k01@gmail.com>
2023-09-14 10:01:42 +0200

    configured global scss variables

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<script setup>
+<script>
 import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
 <template>
   <Header />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+#menu_list {
+  display: flex;
+
+  li {
+    list-style: none;
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,59 @@
-<script setup>
-import { ref } from "vue";
+<script>
+console.log("WHAT");
 </script>
 
-<template></template>
+<template>
+  <header class="container-fluid mx-auto p-0 bg-success">
+    <div class="container d-flex justify-content-around p-0 bg-warning">
+      <div id="logo">
+        <img src="\img\dc-logo.png" alt="dc_logo" />
+      </div>
+
+      <div>
+        <ul id="menu_list" class="mb-0">
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
 
 <style lang="scss" scoped>
-.read-the-docs {
-  color: #888;
+header {
+  height: 150px;
+
+  .container {
+    height: 100%;
+  }
+}
+
+#logo {
+  width: 125px;
+  aspect-ratio: 1;
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 100%;
+  }
+}
+
+#menu_list {
+  gap: 10px;
+  height: 100%;
+
+  li {
+    display: inline-block;
+    height: 100%;
+    border-bottom: 10px $accent-color solid;
+    font-family: none;
+  }
 }
 </style>
