5ca7951cfb8ccf5bdd41cf8acdc5f4c149c0a38c
plastikSun <marcom2k01@gmail.com>
2023-09-14 17:13:55 +0200

    add: main container

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,25 @@ export default {
 
 <template>
   <Header />
+  <main id="main_content">
+    <div class="container__main">
+      <h1 class="text-white">TEST</h1>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
+#main_content {
+  background-color: #1c1c1c;
+  min-height: 200px;
+
+  .container__main {
+    margin: auto;
+    width: 90%;
+    padding: 2em 0;
+  }
+}
+
 #menu_list {
   display: flex;
 
